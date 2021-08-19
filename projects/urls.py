0b154b69731e910338da31846e2f2d0112f562e5
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-08-19 19:48:02 +0100

    all files for projects app

---
+++ projects/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.projects, name='projects'),
+    path('project/<str:pk>/', views.project, name='project'),
+    path('create_project/', views.createProject, name='create_project'),
+    path('update_project/<str:pk>/', views.updateProject, name='update_project'),
+    path('delete_project/<str:pk>/', views.deleteProject, name='delete_project'),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Project
+from .forms import ProjectForm
 
 # Create your views here.
+def projects(request):
+    all_projects = Project.objects.all()
+    context = {
+        'projects': all_projects,
+    }
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObt = Project.objects.get(id=pk)
+    return render(request, 'projects/single_project.html', {'project': projectObt})
+
+@login_required(login_url='login')
+def createProject(request):
+    profile = request.user.profile
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+            return redirect('user_account')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'projects/project_form.html', context)
+
+@login_required(login_url='login')
+def updateProject(request, pk):
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('user_account')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'projects/project_form.html', context)
+
+@login_required(login_url='login')
+def deleteProject(request, pk):
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('user_account')
+    context = {
+        'object': project,
+    }
+    return render(request, 'delete_template.html', context)
+
+
+
